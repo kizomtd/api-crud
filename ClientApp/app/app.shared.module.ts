dc01fbd5d1fd870674b3917a1ebcea8cc6b40b84
Rodrigo Cardoso <rodrigo.ti@mfrural.com.br>
2018-03-08 11:30:32 -0300

    cliente.component

--- ClientApp/app/app.shared.module.ts
+++ ClientApp/app/app.shared.module.ts
@@ -7,15 +7,17 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
-import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
-import { CounterComponent } from './components/counter/counter.component';
+//import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
+//import { CounterComponent } from './components/counter/counter.component';
+import { ClienteComponent } from './components/cliente/cliente.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         NavMenuComponent,
-        CounterComponent,
-        FetchDataComponent,
+        ClienteComponent,
+        //CounterComponent,
+        //FetchDataComponent,
         HomeComponent
     ],
     imports: [
@@ -25,8 +27,9 @@ import { CounterComponent } from './components/counter/counter.component';
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
+            { path: 'clientes', component: ClienteComponent },
+            //{ path: 'counter', component: CounterComponent },
+            //{ path: 'fetch-data', component: FetchDataComponent },
             { path: '**', redirectTo: 'home' }
         ])
     ]
